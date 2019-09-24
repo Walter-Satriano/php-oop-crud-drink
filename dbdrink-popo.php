ec5cd0d34677b193f51c8acfbf39b942d08279f0
Walter Satriano <walter.satri@gmail.com>
2019-09-24 15:21:17 +0200

    definizione Classe e relative funzioni

--- dbdrink-popo.php
+++ dbdrink-popo.php
@@ -1,6 +1,80 @@
 <?php
 
+  class Bevande {
 
+    private $id;
+    private $nome;
+    private $marca;
+    private $prezzo;
+    private $scandenza;
 
+    public function __construct($row) {
+
+      $this -> setId($row["id"]);
+      $this -> setNome($row["nome_bevanda"]);
+      $this -> setMarca($row["marca"]);
+      $this -> setPrezzo($row["prezzo"]);
+      $this -> setScadenza($row["data_di_scadenza"]);
+    }
+
+    public function getId() {
+
+      return $this -> id;
+    }
+
+    public function setId($id) {
+
+      $this -> id = $id;
+    }
+
+    public function getNome() {
+
+      return $this -> nome;
+    }
+
+    public function setNome($nome) {
+
+      $this -> nome = $nome;
+    }
+
+    public function getMarca() {
+
+      return $this -> marca;
+    }
+
+    public function setMarca($marca) {
+
+      $this -> marca = $marca;
+    }
+
+    public function getPrezzo() {
+
+      return $this -> prezzo;
+    }
+
+    public function setPrezzo($prezzo) {
+
+      $this -> prezzo = $prezzo;
+    }
+
+    public function getScadenza() {
+
+      return $this -> scadenza;
+    }
+
+    public function setScadenza($scadenza) {
+
+      $this -> scadenza = $scadenza;
+    }
+
+    public function __toString() {
+
+      return "ID: " . $this -> getId() . " -- "
+           . "NOME: " . $this -> getNome() . " -- "
+           . "MARCA: " . $this -> getMarca() . " -- "
+           . "PREZZO: " . $this -> getPrezzo() . " -- "
+           . "SCADENZA: " . $this -> getScadenza();
+    }
+  }
 
 ?>
