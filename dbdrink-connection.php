ebfe0064ce28bbd6125f6a9e2f78bd3f48095702
Walter Satriano <walter.satri@gmail.com>
2019-09-24 15:19:51 +0200

    connessione al database

--- dbdrink-connection.php
+++ dbdrink-connection.php
@@ -1,6 +1,17 @@
 <?php
 
+  $servername = 'localhost';
+  $username = 'root';
+  $password = 'root';
+  $dbname = 'drinkdb';
 
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
+  if ($conn -> connect_error) {
+
+    var_dump('error');
+    var_dump($conn);
+    die();
+  }
 
 ?>
