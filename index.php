80b76eaa694655927bb31dc0ce48eb732be7397b
Walter Satriano <walter.satri@gmail.com>
2019-09-24 15:45:29 +0200

    download e stampa dei dati

--- index.php
+++ index.php
@@ -1,5 +1,55 @@
 <?php
 
+  include "dbdrink-popo.php";
+  include "dbdrink-query.php";
+  include "dbdrink-connection.php";
 
+  $res = $conn -> query($query_getBevande);
+
+  $bevande = [];
+  if ($res && $res -> num_rows > 0) {
+
+    while ($row = $res -> fetch_assoc()) {
+
+      $bevande[] = new Bevande($row);
+    }
+  }
+
+  for ($i=0; $i < sizeof($bevande); $i++) {
+
+    $bevanda = $bevande[$i];
+    echo $bevanda . "<br><br>";
+  }
+
+  echo "<br>--------- PREZZO MIN / PREZZO MAX / SOMMA PREZZI ---------<br><br>";
+
+  $min = $bevande[0];
+  $max = $bevande[0];
+  $omma = 0;
+
+  for ($i=0; $i < sizeof($bevande); $i++) {
+
+    $bevanda = $bevande[$i];
+
+    $prezzoBevanda = $bevanda -> getPrezzo();
+    $prezzoMin = $min -> getPrezzo();
+    $prezzoMax = $max -> getPrezzo();
+
+    $somma += $prezzoBevanda;
+
+    if ($prezzoMin > $prezzoBevanda) {
+
+      $min = $bevanda;
+    }
+
+    if ($prezzoMax < $prezzoBevanda) {
+
+      $max = $bevanda;
+    }
+  }
+
+  echo "MIN: " . $min . "<br><br>"
+     . "MAX: " . $max . "<br><br>"
+     . "SUM: " . $somma;
 
 ?>
